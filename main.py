b1b535323455b5bf77974afe8f6347e4d39da07e
MoinulAx <113442311+MoinulAx@users.noreply.github.com>
2022-12-15 09:26:08 -0500

    aed

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-mn
+from collections import *
+NASA_file= open("NASA_access_log_Jul95")
+
+IPaddresses=[]
+
+
+try:
+    for lines in NASA_file:
+        
+        IPaddresses.append(lines.split(" ")[0])
+        
+
+
+except:
+    print("uh-oh")
+
+
+print(Counter(IPaddresses))
